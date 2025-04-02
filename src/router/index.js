c04e82790198b624d7138b68450b325c1558ced8
Sahil Kumar <sahilsync07@gmail.com>
2025-04-02 12:10:09 +0530

    Fixed GitHub Pages routing and image issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Doctors from '../views/Doctors.vue'
 import Hospitals from '../views/Hospitals.vue'
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
